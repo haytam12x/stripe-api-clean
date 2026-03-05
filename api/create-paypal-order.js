b33931baf304f41396153e886c6d7d11647a87b5
haytam12x <2017haytam@gmail.com>
2026-03-05 03:18:41 +0000

    Update create-paypal-order.js

--- api/create-paypal-order.js
+++ api/create-paypal-order.js
@@ -37,8 +37,43 @@ export default async function handler(req, res) {
   const accessToken = tokenData.access_token
 
   const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body
-  const price = body.price
   let currency = body.currency
+let price = Number(body.price)
+
+if(currency === "INR"){
+  currency = "USD"
+  price = (price * 0.012).toFixed(2)
+}
+
+if(currency === "PKR"){
+  currency = "USD"
+  price = (price * 0.0036).toFixed(2)
+}
+
+if(currency === "BDT"){
+  currency = "USD"
+  price = (price * 0.0091).toFixed(2)
+}
+
+if(currency === "NGN"){
+  currency = "USD"
+  price = (price * 0.00063).toFixed(2)
+}
+
+if(currency === "VND"){
+  currency = "USD"
+  price = (price * 0.000041).toFixed(2)
+}
+
+if(currency === "IDR"){
+  currency = "USD"
+  price = (price * 0.000064).toFixed(2)
+}
+
+if(currency === "EGP"){
+  currency = "USD"
+  price = (price * 0.020).toFixed(2)
+}
 
   /* PAYPAL UNSUPPORTED CURRENCIES FALLBACK */
 
