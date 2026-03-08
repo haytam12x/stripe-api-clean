6ca831ac3a498f452ecc28e2f37e3730867eed93
haytam12x <2017haytam@gmail.com>
2026-03-08 18:29:42 +0000

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
       : Math.round(Number(amount) * 100);
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+      payment_method_types: ["card", "apple_pay", "google_pay", "cashapp"],
       line_items: [
         {
           price_data: {
